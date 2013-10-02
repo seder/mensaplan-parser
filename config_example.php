fdd31030e75c4cd3f02bd3441209fd528601b4aa
Thomas Lukaseder <thomas.lukaseder@uni-ulm.de>
2013-10-02 12:11:22 +0200

    added example for config.php

---
+++ config_example.php
@@ -0,0 +1,8 @@
+<?php  
+  // path for wget
+  $pfad = "./";//"/users/student1/s_sfuchs/public_html/mensaplan-parser/";
+
+  // save XML and JSON to this directory
+  $outputDir = "./";//"/soft/www/root/mensaplan/data/";
+
+  $elementToFind = "P";
