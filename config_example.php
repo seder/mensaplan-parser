1a8d38563c1e3a04562d918e754cf052e7b9491e
Thomas Lukaseder <thomas.lukaseder@uni-ulm.de>
2013-10-24 18:47:41 +0200

    xml-parser does not need that

--- config_example.php
+++ config_example.php
@@ -5,8 +5,3 @@
   // save XML and JSON to this directory
   $outputDir = "./";//"/soft/www/root/mensaplan/data/";
 
-  /*
-   * There are different implementations of pdftohtml, for some you need "DIV" 
-   * here, for some "P".
-   */ 
-  $elementToFind = "P";//"DIV";
